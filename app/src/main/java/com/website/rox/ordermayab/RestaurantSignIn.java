484f8b7d5f5f5f7f81208ec552ba81fc80790ce7
rlemchar <lemcharkiroxane@gmail.com>
2017-11-27 22:23:39 -0600

    commit before recloning

--- app/src/main/java/com/website/rox/ordermayab/RestaurantSignIn.java
+++ app/src/main/java/com/website/rox/ordermayab/RestaurantSignIn.java
@@ -3,7 +3,9 @@ package com.website.rox.ordermayab;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 public class RestaurantSignIn extends AppCompatActivity {
 
@@ -11,7 +13,15 @@ public class RestaurantSignIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_sign_in);
-    }
+/*
+        // morning_from spinner
+        Spinner morning_from_spinner = (Spinner) findViewById(R.id.morning_from_spinner);
+        ArrayAdapter<CharSequence> career_spinner_adapter = ArrayAdapter.createFromResource(this,
+                R.array.career, android.R.layout.simple_spinner_item);
+        career_spinner_adapter.setDropDownViewResource(
+                android.R.layout.simple_spinner_dropdown_item);
+        career_spinner.setAdapter(career_spinner_adapter);
+    */}
 
     public void addARestaurant(View view){
         EditText name_EditText = (EditText)findViewById(R.id.name_restaurant_editText);
