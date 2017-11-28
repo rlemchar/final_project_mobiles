4f10b941b34d208ca6f67481d469c82b9882715e
rlemchar <lemcharkiroxane@gmail.com>
2017-11-27 22:13:49 -0600

    added - spinners in add client

--- app/src/main/java/com/website/rox/ordermayab/ClientSignIn.java
+++ app/src/main/java/com/website/rox/ordermayab/ClientSignIn.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class ClientSignIn extends AppCompatActivity {
 
@@ -11,6 +13,22 @@ public class ClientSignIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_sign_in);
+
+        // Semester spinner
+        Spinner semester_spinner = (Spinner) findViewById(R.id.semester_spinner);
+        ArrayAdapter<CharSequence> semester_spinner_adapter = ArrayAdapter.createFromResource(this,
+                R.array.semester, android.R.layout.simple_spinner_item);
+        semester_spinner_adapter.setDropDownViewResource(
+                android.R.layout.simple_spinner_dropdown_item);
+        semester_spinner.setAdapter(semester_spinner_adapter);
+
+        // Career spinner
+        Spinner career_spinner = (Spinner) findViewById(R.id.career_spinner);
+        ArrayAdapter<CharSequence> career_spinner_adapter = ArrayAdapter.createFromResource(this,
+                R.array.career, android.R.layout.simple_spinner_item);
+        career_spinner_adapter.setDropDownViewResource(
+                android.R.layout.simple_spinner_dropdown_item);
+        career_spinner.setAdapter(career_spinner_adapter);
     }
 
     public void addAClient(View view){
--- app/src/main/java/com/website/rox/ordermayab/RestaurantSignIn.java
+++ app/src/main/java/com/website/rox/ordermayab/RestaurantSignIn.java
@@ -2,8 +2,8 @@ package com.website.rox.ordermayab;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class RestaurantSignIn extends AppCompatActivity {
 
@@ -14,6 +14,8 @@ public class RestaurantSignIn extends AppCompatActivity {
     }
 
     public void addARestaurant(View view){
-
+        EditText name_EditText = (EditText)findViewById(R.id.name_restaurant_editText);
+        EditText manager_EditText = (EditText)findViewById(R.id.manager_restaurant_editText);
+        EditText cuisine_EditText = (EditText)findViewById(R.id.cuisine_restaurant_editText);
     }
 }
