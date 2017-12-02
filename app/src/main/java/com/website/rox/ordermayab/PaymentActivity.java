2f693ee175f7381e099a8b09e89942216a16024e
broslaura <bros.laura@gmail.com>
2017-12-01 23:02:42 -0600

    payment

---
+++ app/src/main/java/com/website/rox/ordermayab/PaymentActivity.java
@@ -0,0 +1,15 @@
+package com.website.rox.ordermayab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PaymentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment);
+    }
+
+
+}
